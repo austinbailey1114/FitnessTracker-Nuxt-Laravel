6d25048b795e0b82b02b4f8e26ee1c23e0354ab5
Austin Bailey <baileyau@bc.edu>
2018-07-18 12:36:25 -0400

    Login page CSS done

--- client/pages/auth/signin.vue
+++ client/pages/auth/signin.vue
@@ -1,6 +1,22 @@
 <template lang="html">
     <div class="">
-        
+        <div class="container has-text centered">
+            <div class="column is-two-fifths centered">
+                <form @submit.prevent="submit()">
+                    <p>Email</p>
+                    <input type="email" name="email" v-model="form.email" class="input is-large" :class="{ 'is-danger': errors.email }">
+                    <p class="help is-danger" v-if="errors.email">
+                        {{ errors.email[0] }}
+                    </p>
+                    <p>Password</p>
+                    <input type="password" name="password" v-model="form.password" class="input is-large" :class="{ 'is-danger': errors.password }">
+                    <p class="help is-danger" v-if="errors.password">
+                        {{ errors.password[0] }}
+                    </p>
+                    <button class="button">Login</button>
+                </form>
+            </div>
+        </div>
     </div>
 </template>
 
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,35 +1,55 @@
 <template>
     <div class="">
-        <div v-if="!authenticated">
-            <div class="container has-text centered">
-                <div class="column is-two-fifths centered">
+        <div v-if="!authenticated" class="auth-parent">
+            <div class="auth-container">
+                <div class="auth-left">
+                    <div class="auth-image-container">
+                        <img src="/hugeIcon.png" height="75" class="auth-image">
+                    </div>
                     <form @submit.prevent="submit()">
-                        <p>Email</p>
-                        <input type="email" name="email" v-model="form.email" class="input is-large" :class="{ 'is-danger': errors.email }">
+                        <p class="auth-prompt">Email</p>
+                        <input type="email" name="email" v-model="form.email" class="auth-input" :class="{ 'is-danger': errors.email }" placeholder="example@example.com">
                         <p class="help is-danger" v-if="errors.email">
                             {{ errors.email[0] }}
                         </p>
-                        <p>Password</p>
-                        <input type="password" name="password" v-model="form.password" class="input is-large" :class="{ 'is-danger': errors.password }">
+                        <p class="auth-prompt">Password</p>
+                        <input type="password" name="password" v-model="form.password" class="auth-input" :class="{ 'is-danger': errors.password }" placeholder="****">
                         <p class="help is-danger" v-if="errors.password">
                             {{ errors.password[0] }}
                         </p>
-                        <button class="button">Login</button>
+                        <div class="auth-submit-container">
+                            <button class="auth-submit">Login</button>
+                        </div>
+                        <div class="auth-bottom-link">
+                            <nuxt-link :to="{ name: 'auth-signup' }" class="auth-link">
+                                <u>Create Account</u>
+                            </nuxt-link>
+                        </div>
                     </form>
                 </div>
-            </div>
-            <nuxt-link :to="{ name: 'auth-signup' }" class="button is-text">
-                Sign up
-            </nuxt-link>
+                <div class="auth-right">
+                    <div class="background">
 
+                    </div>
+                    <div class="garble">
+                        <p class="auth-info">Track Nutrition, Weight Lifting, and Bodyweight with Fitness Tracker</p>
+                    </div>
+
+                    <!-- <p class="auth-header">About Fitness Tracker</p>
+                    <ul class="list">
+                        <li class="list-item">Track Weight Lifting Data</li>
+                        <li class="list-item">Log Meals From Over 8000 Foods</li>
+                        <ul class="list">
+                            <li class="list-item">USDA Database</li>
+                            <li class="list-item">Track Daily Macronutrients</li>
+                        </ul>
+                        <li class="list-item">Track Bodyweight</li>
+                    </ul> -->
+                </div>
+            </div>
         </div>
         <div v-else>
-            <a @click.prevent="logout" class="button is-text">Sign out</a>
-            {{ authenticated }}
-            {{ user }}
-            <nuxt-link :to="{ name: 'dashboard' }" class="button is-text">
-                Dashboard
-            </nuxt-link>
+
         </div>
     </div>
 </template>
